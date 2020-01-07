91d1e073f11dea59cbdbe7e1a3a0e381a1fc848c
Marcos Toranzo Alfonso <m.toranzo@estudiantes.matcom.uh.cu>
2020-01-06 21:38:08 -0500

    Final++

--- core/utils/manager.py
+++ core/utils/manager.py
@@ -14,7 +14,8 @@ class SummaryManager:
         self.__raw_sentence_by_processed_sentence = None
 
     def preprocess(self, text):
-        doc = re.sub(r'(¿|¡)(\w|\s)*(\?|!)', '', text)
+        doc = re.sub(r'(\n|\r)', '', text)
+        doc = re.sub(r'(¿|¡)(\w|\s)*(\?|!)', '', doc)
         self.raw_sentences = [x for x in doc.split('.') if len(x) > 2]
 
         doc = re.sub(r'("|-|:|,)', '', doc)
